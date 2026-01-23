7062d5de95638ff47eb5883d665fd94f8dc71238
ManojPokuru <manojp@unifytech.com>
2026-01-23 17:01:34 +0530

    plugin changes

--- tutor_openedx_ai_course_creator/ai_course_creator/apps.py
+++ tutor_openedx_ai_course_creator/ai_course_creator/apps.py
@@ -4,18 +4,11 @@ from django.apps import AppConfig
 class AiCourseCreatorConfig(AppConfig):
     name = "ai_course_creator"
     verbose_name = "AI Course Creator"
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = "django.db.models.BigAutoField"
 
     plugin_app = {
         "settings_config": {
-            "lms.djangoapp": {
-                "common": {
-                    "INSTALLED_APPS": [
-                        "ai_course_creator",
-                    ],
-                },
-            },
-            "cms.djangoapp": {
+            "cms": {
                 "common": {
                     "INSTALLED_APPS": [
                         "ai_course_creator",
@@ -24,7 +17,7 @@ class AiCourseCreatorConfig(AppConfig):
             },
         },
         "urls_config": {
-            "cms.djangoapp": {
+            "cms": {
                 "namespace": "ai_course_creator",
                 "regex": r"^ai-course-creator/",
                 "relative_path": "urls",
@@ -33,5 +26,4 @@ class AiCourseCreatorConfig(AppConfig):
     }
 
     def ready(self):
-        """Initialize the plugin"""
         pass
--- tutor_openedx_ai_course_creator/hooks.py
+++ tutor_openedx_ai_course_creator/hooks.py
@@ -1,8 +1,5 @@
 from tutor import hooks
 
-# --------------------------------
-# Config defaults
-# --------------------------------
 @hooks.Filters.CONFIG_DEFAULTS.add()
 def _add_ai_course_creator_defaults(items):
     items.append(("AI_COURSE_CREATOR_GEMINI_API_KEY", ""))
@@ -10,78 +7,12 @@ def _add_ai_course_creator_defaults(items):
     return items
 
 
-# --------------------------------
-# Install plugin from GitHub
-# --------------------------------
 @hooks.Filters.ENV_PATCHES.add()
 def install_ai_course_creator(patches):
     patches.append((
         "openedx-dockerfile-post-python-requirements",
         """
 RUN /openedx/venv/bin/pip install git+https://github.com/ManojPokuru/course-creator-plugin.git
-"""
-    ))
-    return patches
-
-# --------------------------------
-# LMS Django settings
-# --------------------------------
-@hooks.Filters.ENV_PATCHES.add()
-def _patch_lms_settings(patches):
-    patches.append((
-        "openedx-lms-settings",
-        """
-INSTALLED_APPS += ["ai_course_creator"]
-"""
-    ))
-    return patches
-
-
-# --------------------------------
-# CMS Django settings
-# --------------------------------
-@hooks.Filters.ENV_PATCHES.add()
-def _patch_cms_settings(patches):
-    patches.append((
-        "openedx-cms-settings",
-        """
-INSTALLED_APPS += ["ai_course_creator"]
-"""
-    ))
-    return patches
-
-
-# --------------------------------
-# LMS URLs
-# --------------------------------
-@hooks.Filters.ENV_PATCHES.add()
-def _patch_lms_urls(patches):
-    patches.append((
-        "openedx-lms-urls",
-        """
-from django.urls import include, path
-
-urlpatterns += [
-    path("", include("ai_course_creator.urls")),
-]
-"""
-    ))
-    return patches
-
-
-# --------------------------------
-# CMS URLs (Tutor 20 style)
-# --------------------------------
-@hooks.Filters.ENV_PATCHES.add()
-def _patch_cms_urls(patches):
-    patches.append((
-        "openedx-cms-urls",
-        """
-from django.urls import include, path
-
-urlpatterns += [
-    path("", include("ai_course_creator.urls")),
-]
 """
     ))
     return patches
