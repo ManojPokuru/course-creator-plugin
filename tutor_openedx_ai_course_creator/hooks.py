ce75f5d2035cb58f440beb95398c470942545c6a
ManojPokuru <manojp@unifytech.com>
2026-01-27 10:34:50 +0530

    plugin changes

--- tutor_openedx_ai_course_creator/hooks.py
+++ tutor_openedx_ai_course_creator/hooks.py
@@ -1,18 +1,39 @@
+"""
+Tutor plugin for AI Course Creator
+"""
+import os
 from tutor import hooks
 
-@hooks.Filters.CONFIG_DEFAULTS.add()
-def _add_ai_course_creator_defaults(items):
-    items.append(("AI_COURSE_CREATOR_GEMINI_API_KEY", ""))
-    items.append(("AI_COURSE_CREATOR_TAVILY_API_KEY", ""))
-    return items
+# Define template folder for this plugin
+template_folder = os.path.join(os.path.dirname(__file__), "templates")
 
+# Register template roots
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(template_folder)
 
-@hooks.Filters.ENV_PATCHES.add()
-def install_ai_course_creator(patches):
-    patches.append((
+# Register template targets - render templates to the environment
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_item(
+    ("ai_course_creator/build", "plugins")
+)
+
+# Install the Django app into Open edX
+hooks.Filters.ENV_PATCHES.add_item(
+    (
         "openedx-dockerfile-post-python-requirements",
         """
-RUN /openedx/venv/bin/pip install git+https://github.com/ManojPokuru/course-creator-plugin.git
-"""
-    ))
-    return patches
+RUN pip install -e /openedx/plugins/ai_course_creator
+""",
+    )
+)
+
+# Add configuration defaults
+hooks.Filters.CONFIG_DEFAULTS.add_item(
+    ("AI_COURSE_CREATOR_GEMINI_API_KEY", "")
+)
+hooks.Filters.CONFIG_DEFAULTS.add_item(
+    ("AI_COURSE_CREATOR_TAVILY_API_KEY", "")
+)
+
+# Add public settings
+hooks.Filters.APP_PUBLIC_SETTINGS.add_item(
+    ("AI_COURSE_CREATOR_ENABLED", True)
+)
