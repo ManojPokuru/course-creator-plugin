39c9b263afcf0022b5b72e55568daa9736d57d2b
ManojPokuru <manojp@unifytech.com>
2026-01-27 10:37:27 +0530

    plugin changes

--- tutor_openedx_ai_course_creator/hooks.py
+++ tutor_openedx_ai_course_creator/hooks.py
@@ -29,11 +29,7 @@ RUN pip install -e /openedx/plugins/ai_course_creator
 hooks.Filters.CONFIG_DEFAULTS.add_item(
     ("AI_COURSE_CREATOR_GEMINI_API_KEY", "")
 )
+
 hooks.Filters.CONFIG_DEFAULTS.add_item(
     ("AI_COURSE_CREATOR_TAVILY_API_KEY", "")
-)
-
-# Add public settings
-hooks.Filters.APP_PUBLIC_SETTINGS.add_item(
-    ("AI_COURSE_CREATOR_ENABLED", True)
 )
