a862b1ede4a38da2855d9e7ed50be7b070a9815e
ManojPokuru <manojp@unifytech.com>
2026-01-27 10:50:16 +0530

    plugin changes

--- tutor_openedx_ai_course_creator/hooks.py
+++ tutor_openedx_ai_course_creator/hooks.py
@@ -1,31 +1,5 @@
-"""
-Tutor plugin for AI Course Creator
-"""
-import os
 from tutor import hooks
 
-# Define template folder for this plugin
-template_folder = os.path.join(os.path.dirname(__file__), "templates")
-
-# Register template roots
-hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(template_folder)
-
-# Register template targets - render templates to the environment
-hooks.Filters.ENV_TEMPLATE_TARGETS.add_item(
-    ("ai_course_creator/build", "plugins")
-)
-
-# Install the Django app into Open edX
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-dockerfile-post-python-requirements",
-        """
-RUN pip install -e /openedx/plugins/ai_course_creator
-""",
-    )
-)
-
-# Add configuration defaults
 hooks.Filters.CONFIG_DEFAULTS.add_item(
     ("AI_COURSE_CREATOR_GEMINI_API_KEY", "")
 )
