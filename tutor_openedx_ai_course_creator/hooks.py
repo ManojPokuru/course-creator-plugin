9af5fd2977507e86a34029d569f5fc2bbb699adc
ManojPokuru <manojp@unifytech.com>
2026-01-27 17:41:19 +0530

    plugin changes

--- tutor_openedx_ai_course_creator/hooks.py
+++ tutor_openedx_ai_course_creator/hooks.py
@@ -1,18 +1,6 @@
 from tutor import hooks
 
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "local-docker-compose-dev-services",
-        """
-lms:
-  volumes:
-    - C:\\Users\\Unify\\Desktop\\studio-course-creator-plugin:/mnt/ai_course_creator
-cms:
-  volumes:
-    - C:\\Users\\Unify\\Desktop\\studio-course-creator-plugin:/mnt/ai_course_creator
-"""
-    )
-)
+
 
 hooks.Filters.CONFIG_DEFAULTS.add_item(
     ("AI_COURSE_CREATOR_GEMINI_API_KEY", "")
